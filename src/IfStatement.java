099ff93124fca33e8a63b01d37cdda6a461ae547
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-28 13:24:02 -0500

    Class 22 - If statement

---
+++ src/IfStatement.java
@@ -0,0 +1,14 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = true;
+        int fileSent = 3;
+
+        if (isBluetoothEnabled){
+            //Send file
+            fileSent++;
+            System.out.println("File Sent");
+        }
+
+
+    }
+}
