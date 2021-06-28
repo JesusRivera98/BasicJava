a00d523f99b01ecc60bad0e59694f715dec0bef3
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-28 13:38:46 -0500

    Class 24 - Files Scope and else sentence

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,13 +1,23 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSent = 3;
 
         if (isBluetoothEnabled){
             //Send file
             fileSent++;
             System.out.println("File Sent");
+
+            //int i = 0;
+            //i++;
+        } else {
+            fileSent--;
+            System.out.println("Please turn on your bluetooth, to start the transfer");
         }
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSent);
+        //System.out.println(i);
 
 
     }
