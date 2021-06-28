8e3641da16ae1190b5f91f4073aba3a1d8b34cbc
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-28 15:30:39 -0500

    Class 39 - for loop int days

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,7 +1,7 @@
 public class Arrays {
     public static void main(String[] args) {
         String[] androidVersions = new String[17];
-        String days[] = new String[7];
+        int days[] = new int[7];
 
         String[][] cities = new String[4][2];
         /*
@@ -24,8 +24,11 @@ public class Arrays {
         androidVersions[3] = "Donut";
 
 
-        for (int i = 0; i <= androidVersions.length-1; i++) {
+        for (int i = 0; i < androidVersions.length; i++) {
             System.out.println(androidVersions[i]);
+        }
+        for (int i = 0; i < days.length; i++) {
+            System.out.println(days[i]);
         }
 
         /*System.out.println(androidVersions[0]);
