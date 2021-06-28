7a6c6b97ac9a8f3e605906e43b98932aaaa3bb66
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-28 15:12:07 -0500

    Class 36 - arrays

---
+++ src/Arrays.java
@@ -0,0 +1,22 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2];
+        /*
+         * +----------------------------+
+         * |    Country |   City        |
+         * ------------------------------
+         * |    Mexico  |   CDMX        |
+         * |    Mexico  |   Guadalajara |
+         * |    Colombia|   Bogota      |
+         * |    Colombia|   Medellin    |
+         * +----------------------------+
+         */
+
+        int[][][] numbers = new int[2][2][2];
+        int[][][][] numbers4 = new int[2][2][2][2];
+
+    }
+}
